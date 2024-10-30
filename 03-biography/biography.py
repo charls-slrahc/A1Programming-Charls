826ea96a6ea70dbad2bc66c031cd8d46eca3e251
charls-slrahc <charlzbryan19@gmail.com>
2024-10-30 13:16:02 +0400

    hh

--- 03-biography/biography.py
+++ 03-biography/biography.py
@@ -1,6 +1,6 @@
 name = input("please enter your name: ")
-hometown = input("please enter your howntown")
-age = input("please enter your age")
+hometown = input("please enter your howntown:")
+age = input("please enter your age:")
 
 print(f"hello my name is {name}")
 print(f"i am from {hometown}")
