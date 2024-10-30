c68d11192ed338b6eba2c2157944ddef7a3ceda3
charls-slrahc <charlzbryan19@gmail.com>
2024-10-30 14:15:29 +0400

    wwwww

--- 03-biography/biography.py
+++ 03-biography/biography.py
@@ -1,7 +1,26 @@
-name = input("please enter your name: ")
-hometown = input("please enter your howntown:")
-age = input("please enter your age:")
+#user input
 
-print(f"hello my name is {name}")
-print(f"i am from {hometown}")
-print(f"i am {age} years old")
+name = input("Please enter your name (First and last): ")
+hometown = input("Please enter your howntown: ")
+
+#valid number only
+
+while True:
+        age_input = input("Enter your age: ")
+        try:
+            age = int(age_input)  # Attempt to convert age to an integer
+            break  # Exit the loop if conversion is successful
+        except ValueError:
+            print("Invalid input for age. Please enter a numeric value.")
+      
+#user information dictionary
+
+user_info = {
+    "name": name,
+    "hometown": hometown,
+    "age": age
+}
+
+print(f"hello my name is {user_info['name']}")
+print(f"i am from {user_info['hometown']}")
+print(f"i am {user_info['age']} years old")
